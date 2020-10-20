a691b9b1caace2782242df5b04a7c22ce820e1b8
helar89 <helar_duran@hotmail.com>
2020-10-19 23:18:05 -0400

    challenge

---
+++ src/App/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+function Home(){
+    return(
+<div>
+<h2>Thanks for your interest!</h2>
+ <p>We will review your application and contact you for additional information should your
+ background and experience meet the requirements of one of our openings.</p>
+</div>
+
+    )
+}
+
+
+export default Home;
---
+++ src/App/Register.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+export default class Register extends React.Component {
+    render() {
+        const { history } = this.props;
+        return (
+            <Formik
+                initialValues={{ email: '' }}
+                validationSchema={Yup.object().shape({
+                    email: Yup.string()
+                        .email('Email is invalid')
+                        .required('Email is required'),
+                })}
+                onSubmit={(fields,{setSubmitting}) => {
+                    let signUpBtn = document.getElementById('btn-sign-up');
+                    signUpBtn.innerHTML = 'Submiting...';
+                    setTimeout(() => {
+                        history.push('/home');
+                        console.log("Logging in", fields, signUpBtn);
+                        
+                        setSubmitting(false);
+                    }, 2000);
+                    
+                   // alert('SUCCESS!! :-)\n\n' + JSON.stringify(fields, null, 4));
+                    
+                   
+                }}
+                render={({ errors, status, touched }) => (
+
+
+                    <div>
+
+                        <p id="main-text">Prepare for your career with a Project Management, Web-Development, Graphic design, or Digital Marketing Internship at Northen.</p>
+                        <Form>
+
+                            <div className="inputs">
+                                
+                                    <div className="left-col">
+                                     
+                                        <Field name="email" type="text" className={'form-control' + (errors.email && touched.email ? ' is-invalid' : '')} />
+                                        <ErrorMessage name="email" component="div" className="invalid-feedback" />
+                                    </div>
+                                    <div className="right-col">
+                                        <select defaultValue="Your Interests">
+                                            <option>Your Interests</option>
+                                            <option>Web Developer</option>
+                                            <option>Digital Marketing</option>
+                                            <option>UX UI</option>
+                                        </select>
+
+                                    </div>
+                                
+
+                            </div>
+
+                            <div className="form-group">
+                                <button id="btn-sign-up" type="submit" className="">Sign Up Now <i className="material-icons">arrow_right</i></button>
+
+                            </div>
+                        </Form>
+                    </div>
+
+                )}
+            />
+        )
+    }
+}
